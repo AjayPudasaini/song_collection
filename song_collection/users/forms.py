27ff1d3d51d8f251badec75e34af388a29c85f3c
AjayPudasaini <ajayapudasaini999@gmail.com>
2023-09-24 13:38:05 +0545

    #time 50min #comment user register with hash password and email usique validation

--- song_collection/users/forms.py
+++ song_collection/users/forms.py
@@ -1,12 +1,15 @@
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django import forms
 from django.core.validators import RegexValidator
-from bootstrap_datepicker_plus.widgets import DatePickerInput
+
 from song_collection.utils.constant import GENDER_CHOICES
+
+
 class RegisterForm(forms.Form):
-    password_regex = r'^(?=.*[A-Z])(?=.*\d)(?=.*[@])[A-Za-z\d@]{8,}$'
+    password_regex = r"^(?=.*[A-Z])(?=.*\d)(?=.*[@])[A-Za-z\d@]{8,}$"
     password_validator = RegexValidator(
         password_regex,
-        message="Password must be at least 8 characters and contain at least one uppercase letter, one number, and one '@' character."
+        message="Password must be at least 8 characters and contain at least one uppercase letter, one number, and one '@' character.",
     )
 
     first_name = forms.CharField(label="First Name")
@@ -17,23 +20,21 @@ class RegisterForm(forms.Form):
         label="Date of Birth",
         widget=DatePickerInput(),
     )
-    gender = forms.ChoiceField(
-        choices=GENDER_CHOICES,
-        widget=forms.Select
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.Select)
+    address = forms.CharField(
+        label="Address", help_text="Kathmandu, Bhaktpur, Lalitpur ...", max_length=100, required=False
     )
-    address = forms.CharField(label="Address", help_text="Kathmandu, Bhaktpur, Lalitpur ...", max_length=100, required=False)
 
     password = forms.CharField(label="Password", widget=forms.PasswordInput, validators=[password_validator])
     confirm_password = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
 
-
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
 
         if password and confirm_password and password != confirm_password:
-            self.add_error('confirm_password', "Passwords do not match.")
+            self.add_error("confirm_password", "Passwords do not match.")
 
         return cleaned_data
 
--- song_collection/users/views.py
+++ song_collection/users/views.py
@@ -1,19 +1,71 @@
-from django.shortcuts import render, redirect
+from datetime import datetime
+
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+from django.db import connection
+from django.shortcuts import redirect, render
 from django.views.generic import View
-from song_collection.users.forms import RegisterForm, LoginForm
+
+from song_collection.users.forms import LoginForm, RegisterForm
 
 
 class UserRegisterView(View):
     def post(self, request, *args, **kwargs):
         form = RegisterForm(request.POST)
         if form.is_valid():
-            return redirect('user_login')
+            cleaned_data = form.cleaned_data
+            first_name = cleaned_data.get("first_name")
+            last_name = cleaned_data.get("last_name")
+            email = cleaned_data.get("email")
+            phone = cleaned_data.get("phone")
+            date_of_birth = cleaned_data.get("date_of_birth")
+            gender = cleaned_data.get("gender")
+            address = cleaned_data.get("address")
+            password = make_password(cleaned_data.get("password"))
+            is_superuser = False
+            is_staff = False
+            is_active = True
+            current_date = datetime.now()
+
+            with connection.cursor() as cursor:
+                query = """ SELECT COUNT(*) FROM "User" WHERE email = %s """
+                cursor.execute(query, [email])
+                email_count = cursor.fetchone()[0]
+
+            if email_count > 0:
+                form.add_error("email", "This email is already registered.")
+                return render(request, "users/auth/register.html", {"form": form})
+
+            with connection.cursor() as cursor:
+                query = """
+                        INSERT INTO "User"(first_name, last_name, email, phone, date_of_birth, gender, address, password, is_superuser, is_staff, is_active, date_joined, created_at, updated_at)
+                        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        """
+                values = [
+                    first_name,
+                    last_name,
+                    email,
+                    phone,
+                    date_of_birth,
+                    gender,
+                    address,
+                    password,
+                    is_superuser,
+                    is_staff,
+                    is_active,
+                    current_date,
+                    current_date,
+                    current_date,
+                ]
+                cursor.execute(query, values)
+                messages.success(request, "User created success")
+            return redirect("user_login")
         else:
-            return render(request, "users/auth/register.html", {"form": form, "message": "Register"})
+            return render(request, "users/auth/register.html", {"form": form})
 
     def get(self, request, *args, **kwargs):
         form = RegisterForm()
-        context = {"form": form, "message":"Register"}
+        context = {"form": form}
         return render(request, "users/auth/register.html", context)
 
 
@@ -24,7 +76,7 @@ class UserLoginView(View):
             email = form.cleaned_data.get("email")
             password = form.cleaned_data.get("password")
             if email == "ajayapudasaini999@gmail.com" and password == "1530":
-                return redirect('dashboard')
+                return redirect("dashboard")
             else:
                 form.add_error("email", "Invalid email or password")
                 form.add_error("password", "Invalid email or password")
@@ -33,5 +85,5 @@ class UserLoginView(View):
 
     def get(self, request, *args, **kwargs):
         form = LoginForm()
-        context = {"form": form, "message":"Login"}
+        context = {"form": form, "message": "Login"}
         return render(request, "users/auth/login.html", context)
