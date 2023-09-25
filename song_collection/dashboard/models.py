d585f3ae3c47df69b4743b5317fbcfe69b534c85
AjayPudasaini <ajayapudasaini999@gmail.com>
2023-09-25 17:44:36 +0545

    #time 30min #comment created model music

--- song_collection/dashboard/models.py
+++ song_collection/dashboard/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from song_collection.utils.constant import GENDER_CHOICES, YEAR_CHOICES
+from song_collection.utils.constant import GENDER_CHOICES, GENRE_CHOICES, YEAR_CHOICES
 from song_collection.utils.models import AbstractDateTime
 
 
@@ -19,5 +19,18 @@ class Artist(AbstractDateTime):
     )
     no_of_album_released = models.PositiveIntegerField(verbose_name="No of album released", default=1)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = "Artist"
+
+
+class Music(AbstractDateTime):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, verbose_name="Artist")
+    title = models.CharField(max_length=255, verbose_name="Title")
+    album_name = models.CharField(max_length=255, verbose_name="Album Name")
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
+
+    class Meta:
+        db_table = "Music"
--- song_collection/utils/constant.py
+++ song_collection/utils/constant.py
@@ -7,3 +7,11 @@ GENDER_CHOICES = [
 ]
 
 YEAR_CHOICES = [(year, year) for year in range(datetime.date.today().year, 1900, -1)]
+
+GENRE_CHOICES = [
+    ("rnb", "Rhythm and blues"),
+    ("country", "Country"),
+    ("classic", "Classic"),
+    ("rock", "Rock"),
+    ("jazz", "Jazz"),
+]
